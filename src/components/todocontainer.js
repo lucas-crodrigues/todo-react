b6c045bd357f1345002eb32a7410831c94c303ee
lucascr <lcrodrigues1995@gmail.com>
2022-08-08 17:49:46 -0300

    Update todocontainer.js

--- src/components/todocontainer.js
+++ src/components/todocontainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoList from "./todolist";
 import Header from "./header";
+import InputTodo from "./inputtodo";
 
 class TodoContainer extends React.Component {
   state = {
@@ -23,15 +24,51 @@ class TodoContainer extends React.Component {
     ]
   };
 
-  handleChange = () => {
-    console.log('clicked');
+  handleChange = (id) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo
+      }),
+    }))
   };
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+
+  addTodoItem = title => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };  
 
   render() {
     return (
       <div>
         <Header />
-        <TodoList todos={this.state.todos} handleChangeProps={this.handleChange} />
+        <InputTodo addTodoProps={this.addTodoItem}/>
+        <TodoList 
+          todos={this.state.todos} 
+          handleChangeProps={this.handleChange} 
+          deleteTodoProps={this.delTodo}
+        />
       </div>
     );
   };
