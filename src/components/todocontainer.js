c7710364012246d055521638ac7000a81ba90d4a
lucascr <lcrodrigues1995@gmail.com>
2022-08-08 14:22:39 -0300

    Update todocontainer.js

--- src/components/todocontainer.js
+++ src/components/todocontainer.js
@@ -1,11 +1,34 @@
 import React from "react";
 
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+
   render() {
     return (
       <div>
-        <h1>Hello</h1>
-        <p>I am a react component</p>
+        <ul>
+          {this.state.todos.map(todo => (
+            <li>{todo.title}</li>
+          ))}
+        </ul>
       </div>
     );
   };
